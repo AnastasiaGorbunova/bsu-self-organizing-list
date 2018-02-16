9ace4874c29e95be4ca5f2b85a9df76c5bbf910b
Anastasia Gorbunova <nastya_gorbunova_1998@mail.ru>
2018-02-16 21:17:24 +0300

    1

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -14,8 +14,8 @@ class SelfOrganizedList {
     }
 
     insert(data) {
-        if(this.size()){
-            var temp = new Node(data);
+        if(this.length===null){
+                 var temp = new Node(data);
             temp.prev = this.tail;
             this.tail.next = temp;
             this.tail = temp;
@@ -29,25 +29,34 @@ class SelfOrganizedList {
     }
 
     size() {
-  		if(this.head){
+  		if(this.head===null){
   			return 0;
+  		} else {
+  			for (var i=1;this.head.next; i++){
+  				this.head=this.head.next;
+  			}
+  			return i;
   		}
     }
 
     at(index) {
-        if (this.size){
+       if (this.head===null){
             return null;
         } 
     }
 
     findNode(data) {
-
+    	if (this.head===null){
+    		return null;
+    	}
     }
 
     toArray() {
-
+    	var arr=[]
+    	if (this.head===null){
+    		return arr;
+    	} 
     }
-
     removeAt(index) {
 
     }
