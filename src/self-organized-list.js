f9388d1f3c9866e0db52150459fec1a04f1e996b
Anastasia Gorbunova <nastya_gorbunova_1998@mail.ru>
2018-02-19 20:29:26 +0300

    Everything is done

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -15,12 +15,13 @@ class SelfOrganizedList {
 
     insert(data) {
         var temp = new Node(data);
+        var currentNode = this.head;
         this.tail = temp;
         if (this.head == null) {
             this.head = temp;
         } else {
 
-            var currentNode = this.head;
+            
             while (currentNode.next) {
                 currentNode = currentNode.next;
             }
@@ -76,10 +77,37 @@ class SelfOrganizedList {
 
     removeAt(index) {
         var temp = this.head;
-        if (this.head = null){
-            return null;
+         if(index == 0 && this.length == 1)
+        {
+            this.length = 0;
+            return true;
         }
 
+        var i=0;
+        while (i<index) {
+            temp = temp.next;
+            i++;
+            }
+       
+        
+        if(index != this.length-1) {
+        temp.next.prev = temp.prev;
+        }
+        else {
+            temp.prev.next = null;
+            this.tail = temp.prev; 
+        }
+        
+        if(index){
+            temp.prev.next = temp.next;
+        }
+        else
+        {
+            temp.next.prev = null;
+            this.head = temp.next; 
+        }
+
+        this.length--;
     }
 
     moveToFront(node) {
@@ -102,17 +130,17 @@ class SelfOrganizedList {
     }
 
     reorganize(data) {
-        var node = this.findNode(data);
-        if(node == this.head) {
+        var temp = this.findNode(data);
+        if(temp == this.head) {
             return true;
         }
         if(this.length == 0) {
             return false;
         }
-        if(!node) {
+        if(!temp) {
             return false;
         } else {
-            this.moveToFront(node);
+            this.moveToFront(temp);
             return true;
         }
     }
